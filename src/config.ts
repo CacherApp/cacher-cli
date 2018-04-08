a47961555864e1bed155362fc6ca47d71c8d8c07
Rui Jiang <rjiang.bb@gmail.com>
2018-04-08 15:06:45 -0400

    Require SSL for production. Added additional Java extensions.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 export default {
+  env: process.env.CACHER_ENV || 'production',
   appHost: process.env.CACHER_APP_HOST || 'https://app.cacher.io',
   apiHost: process.env.CACHER_API_HOST || 'https://api.cacher.io',
   snippetsHost: process.env.CACHER_SNIPPETS_HOST || 'https://snippets.cacher.io'
